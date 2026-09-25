449bb75e18494f05344ba02dee5274990fb04de3
devwith-rprasit20 <pkcprasit@gmail.com>
2026-09-25 15:50:39 +0530

    All type of Typecasting

--- Typecasting.java
+++ Typecasting.java
@@ -5,8 +5,26 @@ public class Typecasting {
         System.out.println(x);
 
         char Prasit = 'A';
-        int y = (int) Prasit;
+        int y = (int) Prasit; // explicit typecasting
         System.out.println(y);
 
+        char Rim = '3';
+        System.out.println((int) Rim);
+
+        char ph = 'b';
+        System.out.println(ph + 0);
+        char sh = 'b';
+        System.out.println(sh + sh);
+
+        char ah = 'b';
+        System.out.println(ah * ah);
+
+        // integer to charecter
+        int o = 43;
+        char oh = (char) o;
+        System.out.println(oh);
+
+        char kh = 'c';
+        System.out.println((int) kh);
     }
 }
