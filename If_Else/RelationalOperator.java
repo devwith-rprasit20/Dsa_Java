6402671a360e2c0b2670bb4ff7d2a2002bd258c3
devwith-rprasit20 <pkcprasit@gmail.com>
2026-09-25 17:54:26 +0530

    Other Operators

---
+++ If_Else/RelationalOperator.java
@@ -0,0 +1,14 @@
+package If_Else;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        System.out.println(6 > 7);
+        System.out.println(6 < 7);
+        System.out.println(6 <= 6);
+        System.out.println(6 >= 6);
+        System.out.println(6 != 5);
+        System.out.println(6 == 6);
+        ;
+    }
+
+}
