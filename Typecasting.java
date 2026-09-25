c968af29645c49bf851a18abda1db7ad924f7628
devwith-rprasit20 <pkcprasit@gmail.com>
2026-09-25 09:20:57 +0530

    Typecasting

---
+++ Typecasting.java
@@ -0,0 +1,12 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        char ch = 'a';
+        int x = ch;// implicit typecasting
+        System.out.println(x);
+
+        char Prasit = 'A';
+        int y = (int) Prasit;
+        System.out.println(y);
+
+    }
+}
