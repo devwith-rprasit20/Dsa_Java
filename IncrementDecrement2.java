78acbe60de973d649d30f0902d78e763c7aa650c
devwith-rprasit20 <pkcprasit@gmail.com>
2026-09-25 17:42:15 +0530

    increment decrement

---
+++ IncrementDecrement2.java
@@ -0,0 +1,10 @@
+public class IncrementDecrement2 {
+    public static void main(String[] args) {
+        int x = 10;
+        // System.out.println(x++);// post increment
+        // System.out.println(x);
+        System.out.println(++x); // before increment
+        System.out.println(x);
+
+    }
+}
---
+++ IncrementDecrement3.java
@@ -0,0 +1,12 @@
+public class IncrementDecrement3 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x++;
+        System.out.println(x + " " + y);
+
+        int a = 10;
+        int b = ++a;
+        System.out.println(a + " " + b);
+
+    }
+}
